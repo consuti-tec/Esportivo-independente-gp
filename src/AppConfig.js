95fe22e6d14a8397948fbebfba4269b6519a6317
consuti-tec <consuti@consuti.com.br>
2023-05-22 00:19:05 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#E01E17",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/esportivo-independente-gp",
+  UrlPainel: "https://painel.sporti.com.br/",
+  TimeSplashScreen: 3000,
+};
